48a736c30e69f20960cffda97d926339a54ba797
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-09-26 08:49:10 +0200

    Creating private React routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { LogInPage } from "./pages/LoginPage";
 import { UserInfoPage } from "./pages/UserInfoPage";
-import { SignUpPage } from "./pages/SigUpPage";
+import { SignUpPage } from "./pages/SignUpPage";
+import { PrivateRoute } from "./auth/PrivateRoute";
 
 export const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
+        <PrivateRoute path="/" exact>
           <UserInfoPage />
-        </Route>
+        </PrivateRoute>
         <Route path="/login">
           <LogInPage />
         </Route>
