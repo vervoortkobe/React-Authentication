5c66035748415320d8dcbbc23d71690b491936a5
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-09-26 10:53:44 +0200

    Adding a login route to the server

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,4 +1,5 @@
+import { loginRoute } from "./loginRoute";
 import { signUpRoute } from "./signUpRoute";
 import { testRoute } from "./testRoute";
 
-export const routes = [signUpRoute, testRoute];
+export const routes = [loginRoute, signUpRoute, testRoute];
