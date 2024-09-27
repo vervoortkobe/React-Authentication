2fec7e08f03d3a5779d2727a5d1242084a8bbee5
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-09-27 23:00:54 +0200

    Adding a verify email route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,5 +2,12 @@ import { loginRoute } from "./loginRoute";
 import { signUpRoute } from "./signUpRoute";
 import { testRoute } from "./testRoute";
 import { updateUserInfoRoute } from "./updateUserInfoRoute";
+import { verifyEmailRoute } from "./verifyEmailRoute";
 
-export const routes = [loginRoute, signUpRoute, testRoute, updateUserInfoRoute];
+export const routes = [
+  loginRoute,
+  signUpRoute,
+  testRoute,
+  updateUserInfoRoute,
+  verifyEmailRoute,
+];
---
+++ back-end/src/routes/verifyEmailRoute.js
@@ -0,0 +1,36 @@
+import { ObjectID } from "mongodb";
+import jwt from "jsonwebtoken";
+import { getDbConnection } from "../db";
+
+export const verifyEmailRoute = {
+  path: "/api/verify-email",
+  method: "put",
+  handler: async (req, res) => {
+    const { verificationString } = req.body;
+    const db = getDbConnection("react-auth-db");
+    const result = await db.collection("users").findOne({
+      verificationString,
+    });
+
+    if (!result)
+      res
+        .status(401)
+        .json({ message: "The email verification code is invalid" });
+
+    const { _id: id, email, info } = result;
+
+    await db
+      .collection("users")
+      .updateOne({ _id: ObjectID(id) }, { $set: { isVerified: true } });
+
+    jwt.sign(
+      { id, email, isVerified: true, info },
+      process.env.JWT_SECRET,
+      { expiresIn: "2d" },
+      (err, token) => {
+        if (err) return res.sendStatus(500);
+        res.status(200).json({ token });
+      }
+    );
+  },
+};
