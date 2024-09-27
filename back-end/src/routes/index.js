48b3a2c7ebd963074428a2b5f449bd390cec6ebc
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-09-27 22:24:24 +0200

    Adding JWTs to the user info page

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,5 +1,6 @@
 import { loginRoute } from "./loginRoute";
 import { signUpRoute } from "./signUpRoute";
 import { testRoute } from "./testRoute";
+import { updateUserInfoRoute } from "./updateUserInfoRoute";
 
-export const routes = [loginRoute, signUpRoute, testRoute];
+export const routes = [loginRoute, signUpRoute, testRoute, updateUserInfoRoute];
