22cc4adb0e5d108a81004d15219dd49024abb9eb
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-09-28 13:26:03 +0200

    Handling successful OAuth authentication

--- front-end/src/pages/LogInPage.js
+++ front-end/src/pages/LogInPage.js
@@ -2,17 +2,26 @@ import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useToken } from "../auth/useToken";
+import { useQueryParams } from "../util/useQueryParams";
 
 export const LogInPage = () => {
-  const [token, setToken] = useToken();
+  const [, setToken] = useToken();
   const [errorMessage, setErrorMessage] = useState("");
 
   const [emailValue, setEmailValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
 
   const [googleOauthUrl, setGoogleOauthUrl] = useState("");
+  const { token: oauthToken } = useQueryParams();
 
   const history = useHistory();
+
+  useEffect(() => {
+    if (oauthToken) {
+      setToken(oauthToken);
+      history.push("/");
+    }
+  }, [oauthToken, setToken, history]);
 
   useEffect(() => {
     const loadOauthUrl = async () => {
